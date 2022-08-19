b7ecdc2a2e57f45cfe997af50262038ea4dd330a
marksikaundi <emmanuelmark117@gmail.com>
2022-08-19 03:58:53 +0200

    TESTING

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
-class Story {
+class Story_app{
   String id;
   String uri;
   String title;
+  String text;
   String publisher;
   String author;
   String image;
   String date;
 
-Story(this.id, this.uri, this.title, this.publisher, this.author, this.image,this.date);
+  Story_app(this.id, this.uri, this.title, this.publisher, this.author, this.image,
+      this.date);
+
+  Story_app.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    uri = json['uri'];
+    title = json['title'];
+    text = json['text'];
+    publisher = json['publisher'];
+    author = json['author'];
+    date = json['date'];
+  }
 }
